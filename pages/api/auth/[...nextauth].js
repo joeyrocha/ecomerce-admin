3945b185d6c809f7692c9b4db6edf190b355bb7f
Joey Rocha <jrochaperez123@gmail.com>
2024-03-05 13:47:04 -0800

    new commits

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
-import NextAuth from 'next-auth'
+import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import {MongoDBAdapter} from "@auth/mongodb-adapter"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from '@/lib/mongodb'
 
-export default NextAuth({
+const adminEmails = ['jrochaperez123@gmail.com']
+
+export const authOptions = {
   providers: [
     // OAuth authentication providers...
 
@@ -12,7 +14,28 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET
     }),
     // Passwordless / email sign in
-   
+
   ],
-  adapter:MongoDBAdapter(clientPromise),
-})
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: ({ session,token,user }) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+
+      }
+    },
+  },
+}
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+}
